f0c1a26a87f399976a4720d8ba176f52ddf613f6
Yuri Raposo <yfr421@ensign.edu>
2026-02-17 18:15:13 -0700

    Add projects page and fix dark mode

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import { ArticleCard } from '@/components/ArticleCard';
 import { Intro } from '@/components/Intro';
 import { SignupWidget } from '@/components/SignUp';
 import { SkillsWidget } from '@/components/SkillsWidget';
+import { WorkWidget } from '@/components/WorkWidget';
 
-export default function Home() {
+export default function Page() {
 	return (
 		<div>
 			<div>Intro</div>
+
+			{/* ARTICLE CARDS FIRST */}
 			<div className='flex flex-col gap-8 lg:flex-row lg:justify-between'>
 				<div className='space-y-8 lg:flex-1'>
 					<ArticleCard
@@ -28,14 +33,55 @@ export default function Home() {
 						url='/articles/sample-article'
 					/>
 				</div>
-				<div className='mx-auto flex w-fit flex-col gap-6'>
+
+				{/*  SIGNUP COLUMN  */}
+				<div className='flex flex-col gap-6'>
+					{/* SIGNUP WIDGET */}
 					<SignupWidget
 						title={'Stay up to date'}
 						content={'Get notified when I publish something new, and unsubscribe at any time.'}
 					/>
+
+					{/* WORK WIDGET */}
+					<WorkWidget
+						title='Work Experience'
+						content='A quick overview of my professional background.'
+						experiences={[
+							{
+								logo: '/Slack.svg',
+								organization: 'Slack',
+								jobTitle: 'Software Engineerr',
+								startYear: 2016,
+								endYear: null,
+							},
+							{
+								logo: '/Spotify.svg',
+								organization: 'Spotify',
+								jobTitle: 'Software Engineer',
+								startYear: 2014,
+								endYear: 2015,
+							},
+							{
+								logo: '/Audible.svg',
+								organization: 'Audible',
+								jobTitle: 'Software Engineer',
+								startYear: 2012,
+								endYear: 2013,
+							},
+							{
+								logo: '/Microsoft.svg',
+								organization: 'Microsoft',
+								jobTitle: 'Software Engineer',
+								startYear: 2010,
+								endYear: 2011,
+							},
+						]}
+					/>
+
+					{/*  SKILLS WIDGET */}
 					<SkillsWidget
-						title={'My Skills'}
-						content={'lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.'}
+						title={'Skills'}
+						content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.'}
 						skills={[
 							{ icon: '/icons/html.svg', name: 'HTML', proficiency: 25 },
 							{ icon: '/icons/css.svg', name: 'CSS', proficiency: 25 },
